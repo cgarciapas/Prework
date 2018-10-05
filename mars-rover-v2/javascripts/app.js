8ac4dcbee1b299c6543158eb205360e6c3be4a6e
Clara Garcia <garciapascual.clara@gmail.com>
2018-10-05 09:32:56 +0200

    first commit

---
+++ mars-rover-v2/javascripts/app.js
@@ -0,0 +1,18 @@
+// Rover Object Goes Here
+// ======================
+var rover = new Object();
+
+var directions = ['N', 'E', 'S', 'W'];
+
+// ======================
+function turnLeft(rover){
+  console.log("turnLeft was called!");
+}
+
+function turnRight(rover){
+  console.log("turnRight was called!");
+}
+
+function moveForward(rover){
+  console.log("moveForward was called")
+}
