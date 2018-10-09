b72e0948d2244b98fe51684b042fece479609b05
Clara Garcia <garciapascual.clara@gmail.com>
2018-10-09 14:42:34 +0200

    rover mars 1,2,3 y 4

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,18 +1,102 @@
 // Rover Object Goes Here
 // ======================
-var rover = new Object();
-
-var directions = ['N', 'E', 'S', 'W'];
+var myRover = {
+  direction: "N",
+  x: 0,
+  y: 0,
+};
 
 // ======================
 function turnLeft(rover){
-  console.log("turnLeft was called!");
+  switch (rover.direction) {
+    case "N":
+    rover.direction = "W";
+    break;
+
+    case "E":
+    rover.direction = "N";
+      break;
+
+    case "S":
+    rover.direction = "E";
+      break;
+
+    case "W":
+    rover.direction = "S";
+    break;
+  }
+  console.log("La dirección de Rover es " + rover.direction);
 }
+
+turnLeft(myRover);
 
 function turnRight(rover){
-  console.log("turnRight was called!");
+  switch (rover.direction) {
+    case "N":
+    rover.direction = "E";
+    break;
+
+    case "E":
+    rover.direction = "S";
+      break;
+
+    case "S":
+    rover.direction = "W";
+      break;
+
+    case "W":
+    rover.direction = "N";
+    break;
+  }
+  console.log("La dirección de Rover es " + rover.direction);
 }
+turnRight(myRover);
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  switch (rover.direction) {
+    case "N":
+      rover.y--;
+      break;
+    
+    case "E":
+      rover.x++;
+      break;
+    
+    case "S":
+      rover.y++;
+      break;
+
+    case "W":
+      rover.x--;
+      break;
+  }
+  console.log("Posición horizontal: " +rover.x);
+  console.log("Posición vertical: " +rover.y);
 }
+
+moveForward(myRover);
+
+
+function listCommands( str ){
+
+  str.split('').forEach( letra => {
+
+    switch (letra) {
+      case 'f':
+        moveForward( myRover );
+      break;
+      case 'l':
+        turnLeft( myRover );
+      break;
+      case 'r':
+        turnRight( myRover );
+      break;
+    }
+  } );
+}
+listCommands('rffrfflfrff');
+
+
+
+
+
